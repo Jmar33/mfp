24dc57c738c5af9ca9a6a9f7ada1e615651425bc
Jmar33 <josimarferreira922@yahoo.com.br>
2024-06-22 05:30:32 -0300

    [feature] Configura ambiente de produção

--- packages/marketing/config/webpack.common.js
+++ packages/marketing/config/webpack.common.js
@@ -8,10 +8,13 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-react', '@babel/preset-env'],
-            plugins: ['@babel/plugin-transform-runtime'],
+            plugins: ['@babel/plugin-transform-runtime', 
+            ],
           }
         }
       }
     ]
-  }
-}
+  },
+};
+
+
