5f911ce05d34ffc4cbf255e800989b6c73637de6
Jmar33 <josimarferreira922@yahoo.com.br>
2024-06-23 08:22:02 -0300

    [feature]: Configura marketing deploy

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -5,6 +5,11 @@ const packageJson = require('../package.json');
 
 const prodConfig = {
   mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js',
+    publicPath: '/marketing/latest/'
+
+  },
   plugins: [
     new ModuleFederationPlugin({
       name: 'marketing',
