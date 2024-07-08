7ead5373a771510d01e91fbc809afbfba94c68d4
Jmar33 <josimarferreira922@yahoo.com.br>
2024-07-08 06:30:43 -0300

    [feature] Adiciona componente auth ao projeto

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
+import AuthApp from "./components/AuthApp";
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co'
@@ -15,7 +16,10 @@ export default () => {
     <StylesProvider generateClassName={generateClassName}>
       <div>
         <Header />
-        <MarketingApp />
+        <Switch>
+          <Route path="/auth" component={AuthApp}></Route>
+          <Route path="/" component={MarketingApp}></Route>
+        </Switch>
       </div>
     </StylesProvider>
   </BrowserRouter>)
